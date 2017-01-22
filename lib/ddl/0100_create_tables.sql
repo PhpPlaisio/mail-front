d3d77d8338b3e6fd3d90c937c3c0793fad979ec5
Paul Water <p.r.water@setbased.nl>
2017-01-22 22:06:13 +0100

    Initial commit.

---
+++ lib/ddl/0100_create_tables.sql
@@ -0,0 +1,146 @@
+/*================================================================================*/
+/* DDL SCRIPT                                                                     */
+/*================================================================================*/
+/*  Title    :                                                                    */
+/*  FileName : mail.ecm                                                           */
+/*  Platform : MySQL 5.6                                                          */
+/*  Version  : Concept                                                            */
+/*  Date     : vrijdag 20 januari 2017                                            */
+/*================================================================================*/
+/*================================================================================*/
+/* CREATE TABLES                                                                  */
+/*================================================================================*/
+
+CREATE TABLE ELM_AUTHORIZED_DOMAIN (
+  atd_domain_name VARCHAR(32) NOT NULL,
+  CONSTRAINT PRIMARY_KEY PRIMARY KEY (atd_domain_name)
+)
+engine=innodb;
+
+CREATE TABLE ELM_HEADER (
+  ehd_id TINYINT UNSIGNED AUTO_INCREMENT NOT NULL,
+  ehd_label VARCHAR(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  CONSTRAINT PRIMARY_KEY PRIMARY KEY (ehd_id)
+)
+engine=innodb;
+
+CREATE TABLE ELM_MESSAGE (
+  elm_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  cmp_id SMALLINT UNSIGNED NOT NULL,
+  blb_id_body INT UNSIGNED NOT NULL,
+  elm_subject VARCHAR(255) NOT NULL,
+  elm_number_from INT NOT NULL,
+  elm_number_to INT NOT NULL,
+  elm_number_cc INT NOT NULL,
+  elm_number_bcc INT NOT NULL,
+  elm_inserted TIMESTAMP DEFAULT current_timestamp NOT NULL,
+  elm_pickuped TIMESTAMP DEFAULT null,
+  elm_sent TIMESTAMP DEFAULT null,
+  CONSTRAINT PRIMARY_KEY PRIMARY KEY (elm_id)
+)
+engine=innodb;
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.blb_id_body
+The BLOB with the email body.
+*/
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.elm_subject
+The subject of this message.
+*/
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.elm_number_from
+The number of From addressees.
+*/
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.elm_number_to
+The number of To addressees.
+*/
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.elm_number_cc
+The number of Cc addressees.
+*/
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.elm_number_bcc
+The number of Bcc addressees.
+*/
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.elm_inserted
+The timestamp when this message was inserted.
+*/
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.elm_pickuped
+The timestamp when this message was pickedup by the delivery process
+*/
+
+/*
+COMMENT ON COLUMN ELM_MESSAGE.elm_sent
+The timestamp when this message was actually delivered to the MTA.
+*/
+
+CREATE TABLE ELM_MESSAGE_HEADER (
+  emh_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  cmp_id SMALLINT UNSIGNED NOT NULL,
+  elm_id INT UNSIGNED NOT NULL,
+  ehd_id TINYINT UNSIGNED NOT NULL,
+  blb_id INT UNSIGNED,
+  usr_id INT UNSIGNED,
+  emh_address VARCHAR(255),
+  emh_name VARCHAR(255),
+  emh_custom_header VARCHAR(255),
+  CONSTRAINT PRIMARY_KEY PRIMARY KEY (emh_id)
+)
+engine=innodb;
+
+/*================================================================================*/
+/* CREATE INDEXES                                                                 */
+/*================================================================================*/
+
+CREATE INDEX IX_FK_ELM_MESSAGE ON ELM_MESSAGE (blb_id_body);
+
+CREATE INDEX IX_FK_ELM_MESSAGE2 ON ELM_MESSAGE (cmp_id);
+
+CREATE INDEX IX_FK_ELM_MESSAGE_HEADER ON ELM_MESSAGE_HEADER (ehd_id);
+
+CREATE INDEX IX_FK_ELM_MESSAGE_HEADER1 ON ELM_MESSAGE_HEADER (usr_id);
+
+CREATE INDEX IX_FK_ELM_MESSAGE_HEADER2 ON ELM_MESSAGE_HEADER (cmp_id);
+
+/*================================================================================*/
+/* CREATE FOREIGN KEYS                                                            */
+/*================================================================================*/
+
+ALTER TABLE ELM_MESSAGE
+  ADD CONSTRAINT FK_ELM_MESSAGE_ABC_BLOB
+  FOREIGN KEY (blb_id_body) REFERENCES ABC_BLOB (blb_id);
+
+ALTER TABLE ELM_MESSAGE
+  ADD CONSTRAINT FK_ELM_MESSAGE_ABC_COMPANY
+  FOREIGN KEY (cmp_id) REFERENCES AUT_COMPANY (cmp_id);
+
+ALTER TABLE ELM_MESSAGE_HEADER
+  ADD CONSTRAINT FK_ELM_ATTRIBUTE_VALUES_ABC_BLOB
+  FOREIGN KEY (blb_id) REFERENCES ABC_BLOB (blb_id);
+
+ALTER TABLE ELM_MESSAGE_HEADER
+  ADD CONSTRAINT FK_ELM_ATTRIBUTE_VALUES_ABC_COMPANY
+  FOREIGN KEY (cmp_id) REFERENCES AUT_COMPANY (cmp_id);
+
+ALTER TABLE ELM_MESSAGE_HEADER
+  ADD CONSTRAINT FK_ELM_ATTRIBUTE_VALUES_ABC_USER
+  FOREIGN KEY (usr_id) REFERENCES AUT_USER (usr_id);
+
+ALTER TABLE ELM_MESSAGE_HEADER
+  ADD CONSTRAINT ELM_ATTRIBUTE_VALUES_ibfk_11
+  FOREIGN KEY (ehd_id) REFERENCES ELM_HEADER (ehd_id);
+
+ALTER TABLE ELM_MESSAGE_HEADER
+  ADD CONSTRAINT ELM_ATTRIBUTE_VALUES_ibfk_10
+  FOREIGN KEY (elm_id) REFERENCES ELM_MESSAGE (elm_id);
---
+++ src/AbcMailMessage.php
@@ -0,0 +1,416 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\Mail;
+
+use SetBased\Abc\Abc;
+use SetBased\Abc\C;
+use SetBased\Exception\LogicException;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * ABC's default implementation of MailMessage.
+ */
+class AbcMailMessage implements MailMessage
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The ID of the BLOB with the body of this message.
+   *
+   * @var int|null
+   */
+  protected $blbId;
+
+  /**
+   * The ID of the company for which this mail will be send.
+   *
+   * @var int
+   */
+  protected $cmpId;
+
+  /**
+   * The single valued headers of this message.
+   *
+   * @var array[]
+   */
+  protected $headers1 = [];
+
+  /**
+   * The list valued headers of this message.
+   *
+   * @var array[]
+   */
+  protected $headers2 = [];
+
+  /**
+   * The subject of this message.
+   *
+   * @var string|null
+   */
+  protected $subject;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Object constructor.
+   *
+   * @param int $cmpId The ID of the company.
+   */
+  public function __construct($cmpId)
+  {
+    $this->cmpId = $cmpId;
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function addBcc($usrId, $address, $name = null)
+  {
+    $this->addHeader(C::EHD_ID_BCC, null, $usrId, $address, $name, null);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function addCc($usrId, $address, $name = null)
+  {
+    $this->addHeader(C::EHD_ID_CC, null, $usrId, $address, $name, null);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function addCustomHeader($name, $value)
+  {
+    if ($name!==null && $value!==null)
+    {
+      $this->addHeader(C::EHD_ID_CUSTOM_HEADER, null, null, null, null, $name.': '.$value);
+    }
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function addFrom($usrId, $address, $name = null)
+  {
+    $this->addHeader(C::EHD_ID_FROM, null, $usrId, $address, $name, null);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function addReadReceiptTo($usrId, $address, $name = null)
+  {
+    $this->addHeader(C::EHD_ID_CONFIRM_READING_TO, null, $usrId, $address, $name, null);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function addReplyTo($usrId, $address, $name = null)
+  {
+    $this->addHeader(C::EHD_ID_REPLY_TO, null, $usrId, $address, $name, null);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function addTo($usrId, $address, $name = null)
+  {
+    $this->addHeader(C::EHD_ID_TO, null, $usrId, $address, $name, null);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function attach($blbId)
+  {
+    $this->addHeader(C::EHD_ID_ATTACHMENT, $blbId, null, null, null, null);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function embed($blbId)
+  {
+    // TODO: Implement embed() method.
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function send()
+  {
+    $count = $this->countAddressees();
+
+    $this->validate($count);
+
+    $transmitter = $this->getTransmitter($count);
+
+    $elm_id = Abc::$DL->abcMailFrontInsertMessage($this->cmpId,
+                                                  $this->blbId,
+                                                  $transmitter['usr_id'],
+                                                  $transmitter['emh_address'],
+                                                  $transmitter['emh_name'],
+                                                  $this->subject,
+                                                  $count['from'],
+                                                  $count['to'],
+                                                  $count['cc'],
+                                                  $count['bcc']);
+
+    foreach ($this->headers1 as $header)
+    {
+      Abc::$DL->abcMailFrontInsertMessageHeader($this->cmpId,
+                                                $elm_id,
+                                                $header['ehd_id'],
+                                                $header['blb_id'],
+                                                $header['usr_id'],
+                                                $header['emh_address'],
+                                                $header['emh_name'],
+                                                $header['emh_custom_header']);
+    }
+
+    foreach ($this->headers2 as $headers)
+    {
+      foreach ($headers as $header)
+      {
+        Abc::$DL->abcMailFrontInsertMessageHeader($this->cmpId,
+                                                  $elm_id,
+                                                  $header['ehd_id'],
+                                                  $header['blb_id'],
+                                                  $header['usr_id'],
+                                                  $header['emh_address'],
+                                                  $header['emh_name'],
+                                                  $header['emh_custom_header']);
+      }
+    }
+
+    return $elm_id;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function setBody($blbId)
+  {
+    $this->blbId = $blbId;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function setMessageId($id)
+  {
+    $this->setHeader(C::EHD_ID_MESSAGE_ID, null, null, null, null, $id);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function setSender($usrId, $address, $name = null)
+  {
+    $this->setHeader(C::EHD_ID_SENDER, null, $usrId, $address, $name, null);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  public function setSubject($subject)
+  {
+    $this->subject = mb_substr($subject, 0, C::LEN_ELM_SUBJECT);
+
+    return $this;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Adds a entry to a header with a list of entries of this message.
+   *
+   * @param int|null    $ehdId   The ID of the attribute.
+   * @param int|null    $blbId   the ID of the BLOB.
+   * @param int|null    $usrId   The ID of the user.
+   * @param string|null $address The address.
+   * @param string|null $name    The name name associated with the address.
+   * @param string|null $header  The custom header.
+   *
+   * @api
+   * @since 1.0.0
+   */
+  protected function addHeader($ehdId, $blbId, $usrId, $address, $name, $header)
+  {
+    $this->validateHeader($address, $header);
+
+    if (!isset($this->headers2[$ehdId]))
+    {
+      $this->headers2[$ehdId] = [];
+    }
+
+    $this->headers2[$ehdId][] = ['ehd_id'            => $ehdId,
+                                 'blb_id'            => $blbId,
+                                 'usr_id'            => $usrId,
+                                 'emh_address'       => $address,
+                                 'emh_name'          => $name,
+                                 'emh_custom_header' => $header];
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Sets a header of this message.
+   *
+   * @param int|null    $ehdId   The ID of the attribute.
+   * @param int|null    $blbId   The ID of the BLOB.
+   * @param int|null    $usrId   The ID of the user.
+   * @param string|null $address The address.
+   * @param string|null $name    The name name associated with the address.
+   * @param string|null $header  The custom header.
+   *
+   * @api
+   * @since 1.0.0
+   */
+  protected function setHeader($ehdId, $blbId, $usrId, $address, $name, $header)
+  {
+    $this->validateHeader($address, $header);
+
+    $this->headers1[$ehdId] = ['ehd_id'            => $ehdId,
+                               'blb_id'            => $blbId,
+                               'usr_id'            => $usrId,
+                               'emh_address'       => $address,
+                               'emh_name'          => mb_strstr($name, 0, C::LEN_EMH_NAME),
+                               'emh_custom_header' => $header];
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns an array with the number of From, To, Cc, and Bcc addresses of this message.
+   *
+   * @return array
+   */
+  private function countAddressees()
+  {
+    return ['from'   => (isset($this->headers2[C::EHD_ID_FROM])) ? count($this->headers2[C::EHD_ID_FROM]) : 0,
+            'to'     => (isset($this->headers2[C::EHD_ID_TO])) ? count($this->headers2[C::EHD_ID_TO]) : 0,
+            'cc'     => (isset($this->headers2[C::EHD_ID_CC])) ? count($this->headers2[C::EHD_ID_CC]) : 0,
+            'bcc'    => (isset($this->headers2[C::EHD_ID_BCC])) ? count($this->headers2[C::EHD_ID_BCC]) : 0,
+            'sender' => (isset($this->headers2[C::EHD_ID_SENDER])) ? 1 : 0];
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the transmitter of this mail message.
+   *
+   * @param array $count The number of From, To, Cc, and Bcc addresses of this message.
+   *
+   * @return array
+   */
+  private function getTransmitter($count)
+  {
+    if ($count['from']==1)
+    {
+      $ehd_id      = C::EHD_ID_FROM;
+      $transmitter = $this->headers2[$ehd_id][0];
+
+      unset($this->headers2[$ehd_id]);
+    }
+    else
+    {
+      $ehd_id      = C::EHD_ID_SENDER;
+      $transmitter = $this->headers1[$ehd_id];
+
+      unset($this->headers1[$ehd_id]);
+    }
+
+    return $transmitter;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Validates this mail messages.
+   *
+   * @param array $count The number of From, To, Cc, and Bcc addresses of this message.
+   */
+  private function validate($count)
+  {
+    if ($count['from']>=2 && $count['sender']==0)
+    {
+      throw new LogicException('Sender is required when mail is send with multiple from addresses.');
+    }
+
+    if ($count['from']<=1 && $count['sender']==1)
+    {
+      throw new LogicException('Sender is required when and only when mail is send with multiple from addresses.');
+    }
+
+    if ($count['to']==0 && $count['cc']==0 && $count['bcc']==0)
+    {
+      throw new LogicException('A delivery end point is mandatory.');
+    }
+
+    if ($this->subject===null)
+    {
+      throw new LogicException('Subject is mandatory.');
+    }
+
+    if ($this->blbId===null)
+    {
+      throw new LogicException('Body is mandatory.');
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Validates the header values.
+   *
+   * @param string|null $address The address.
+   * @param string|null $header  The custom header.
+   */
+  private function validateHeader($address, $header)
+  {
+    if ($address!==null && mb_strlen($address)>C::LEN_EMH_ADDRESS)
+    {
+      throw new LogicException("Length of address '%s' exceeds %d characters.", $address, C::LEN_EMH_ADDRESS);
+    }
+
+    if ($header!==null && mb_strlen($header)>C::LEN_EMH_CUSTOM_HEADER)
+    {
+      throw new LogicException("Length of header '%s' exceeds %d characters.", $header, C::LEN_EMH_CUSTOM_HEADER);
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
